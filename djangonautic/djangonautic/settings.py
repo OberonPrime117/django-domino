14c966d81e0e12e0d552402d3fc46674cb4d22df
OberonPrime117 <71881484+OberonPrime117@users.noreply.github.com>
2021-07-19 22:50:59 +0530

    Update settings.py

--- djangonautic/djangonautic/settings.py
+++ djangonautic/djangonautic/settings.py
@@ -20,14 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5(r+e9^dsa1m9-a$3=83wl%&pbduk-9$u4w^b4k&e46_hu4h75'
+SECRET_KEY = '#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
-STRIPE_SECRET_KEY = 'sk_test_51J8j50SEQQTfbXyAh9UooZrDuLW51H9dmYJKNBR1YpdjChROB9TbC8jodr8TElpW8BJKSoCRZciuXl6YlbeBItWi009ZNmmFHj'
+STRIPE_SECRET_KEY = '#'
 CORS_ORIGIN_ALLOW_ALL=True
 # Application definition
 
